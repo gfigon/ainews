f08581dcdfc95b15a65d9ef165b77a1edf38bc0a
gfigon <grzegorz.figon@gmail.com>
2026-02-14 12:27:16 +0100

    Fix Klaro config - use working version from DomenaRynku with EN translations

--- klaro-config.js
+++ klaro-config.js
@@ -1,25 +1,43 @@
 // Konfiguracja Klaro Cookie Consent dla roboaidigest.com
-
 var klaroConfig = {
     // Wersja językowa
     lang: 'en',
     
-    // Czy użytkownik musi wybrać (true = wymusza wybór)
-    mustConsent: false,
+    // Czy zastosować Consent Mode v2 (Google)
+    mustConsent: true,
     
-    // Czy pokazać przycisk "Accept all"
+    // Czy zgody są opcjonalne (false = użytkownik musi wybrać)
     acceptAll: true,
     
-    // Czy pokazać przycisk "Decline all"  
+    // Czy pokazać toggle dla wszystkich kategorii naraz
     hideDeclineAll: false,
     
-    // Stylizacja
+    // Czy pokazać link do polityki prywatności
+    privacyPolicy: '/privacy',
+    
+    // Ustawienia wyglądu - USUNIĘTE theme, będziemy sterować przez CSS
     styling: {
         theme: ['bottom', 'wide'],
     },
     
-    // Tłumaczenia angielskie
+    // Tłumaczenia
     translations: {
+        en: {
+            consentModal: {
+                title: 'Szanujemy Twoją prywatność',
+                description: 'Używamy plików cookies i podobnych technologii do personalizacji treści, analizy ruchu i reklam. Możesz zarządzać swoimi preferencjami poniżej.',
+            },
+            consentNotice: {
+                description: 'Używamy plików cookies do analizy ruchu, personalizacji treści i wyświetlania reklam. Możesz zarządzać swoimi preferencjami dotyczącymi cookies.',
+                learnMore: 'Dostosuj ustawienia',
+            },
+            ok: 'Akceptuję wybrane',
+            acceptAll: 'Akceptuję wszystkie',
+            acceptSelected: 'Akceptuję wybrane',
+            decline: 'Odrzuć wszystkie',
+            save: 'Zapisz ustawienia',
+            close: 'Zamknij',
+        },
         en: {
             consentModal: {
                 title: 'We value your privacy',
@@ -35,19 +53,96 @@ var klaroConfig = {
                     description: 'Helps us understand how visitors use our website.',
                 },
             },
+            ok: 'OK',
             acceptAll: 'Accept all',
             acceptSelected: 'Accept selected',
-            decline: 'Decline all',
+            decline: 'Decline',
             save: 'Save preferences',
             close: 'Close',
         },
     },
     
-    // Serwisy/cookies (puste - brak zewnętrznych skryptów)
-    services: [],
+    // Definicje poszczególnych serwisów/cookies
+    services: [
+        {
+            name: 'google-analytics',
+            title: 'Google Analytics',
+            purposes: ['analytics'],
+            required: false,
+            optOut: false,
+            default: false,
+            onlyOnce: false,
+            cookies: [
+                [/^_ga/, '/', 'roboaidigest.com'],
+                [/^_gid/, '/', 'roboaidigest.com'],
+            ],
+            callback: function(consent, service) {
+                // Consent Mode v2 dla Google Analytics
+                if (consent) {
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('consent', 'update', {
+                        'analytics_storage': 'granted'
+                    });
+                } else {
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('consent', 'update', {
+                        'analytics_storage': 'denied'
+                    });
+                }
+            },
+        },
+        {
+            name: 'google-ads',
+            title: 'Google Ads',
+            purposes: ['marketing'],
+            required: false,
+            optOut: false,
+            default: false,
+            onlyOnce: false,
+            cookies: [
+                [/^_gcl_/, '/', 'roboaidigest.com'],
+            ],
+            callback: function(consent, service) {
+                // Consent Mode v2 dla Google Ads
+                if (consent) {
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('consent', 'update', {
+                        'ad_storage': 'granted',
+                        'ad_user_data': 'granted',
+                        'ad_personalization': 'granted'
+                    });
+                } else {
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('consent', 'update', {
+                        'ad_storage': 'denied',
+                        'ad_user_data': 'denied',
+                        'ad_personalization': 'denied'
+                    });
+                }
+            },
+        },
+        {
+            name: 'preferences',
+            title: 'Preferencje użytkownika',
+            purposes: ['personalization'],
+            required: false,
+            default: false,
+            cookies: [
+                ['user_preferences', '/', 'roboaidigest.com'],
+            ],
+        },
+    ],
     
-    // Domyślne zgody
+    // Domyślne zgody (przed wyborem użytkownika)
     default: {
         analytics: false,
+        marketing: false,
+        personalization: false,
+        social: false,
     },
 };
+
